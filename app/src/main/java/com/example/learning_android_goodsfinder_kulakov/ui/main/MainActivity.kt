7351648428b24022919d3f316db3f8245a4e2f64
Alexander <alexkulakov01@gmail.com>
2023-05-06 11:25:25 +0300

    add up button to action bar

--- app/src/main/java/com/example/learning_android_goodsfinder_kulakov/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/learning_android_goodsfinder_kulakov/ui/main/MainActivity.kt
@@ -1,12 +1,14 @@
 package com.example.learning_android_goodsfinder_kulakov.ui.main
 
 import android.os.Bundle
+import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.FragmentManager.OnBackStackChangedListener
 import com.example.learning_android_goodsfinder_kulakov.R
 import com.example.learning_android_goodsfinder_kulakov.databinding.ActivityMainBinding
 import com.example.learning_android_goodsfinder_kulakov.ui.good_list.GoodListFragment
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), OnBackStackChangedListener {
 
     private lateinit var binding: ActivityMainBinding
 
@@ -18,7 +20,22 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
             .replace(R.id.fragmentContainerLeft, GoodListFragment())
             .commit()
+
+        supportFragmentManager.addOnBackStackChangedListener(this)
+    }
+
+    override fun onBackStackChanged() {
+        val backStackEntryCount = supportFragmentManager.backStackEntryCount
+        supportActionBar?.setDisplayHomeAsUpEnabled(backStackEntryCount > 0)
     }
 
     fun isLandscapeOrTablet() = binding.fragmentContainerRight != null
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            supportFragmentManager.popBackStack()
+            return true
+        }
+        return false
+    }
 }
